e12102cfcbe9bda76ec29f625e05eb51711ebc77
shashwatkap1 <shashwatkapoor1999@gmail.com>
2021-02-07 04:43:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+
+import React, { useState,useEffect } from 'react'
 import './App.css';
+import Login from "./Login";
+import {getTokenFromUrl} from "./spotify"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token,setToken]=useState();
+  useEffect(() => {
+    const hash= getTokenFromUrl();
+    window.location.hash="";
+    const _token=hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
+  
+
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;  
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Login.css"
+import {loginUrl} from "./spotify"
+function Login() {
+    return (
+        <div className="login">
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" className="logo_big" alt="Spotify logo" />
+            <button > <a href={loginUrl}> LOGIN WITH SPOTIFY </a></button>
+      
+        </div>
+    )
+}
+
+export default Login
